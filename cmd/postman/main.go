e60f0de597d61556d40d1a0d0a96ef06f6651932
Nathan_syr <learncodexx>
2026-04-09 10:41:56 +0700

    [UPDATE] change flow

--- cmd/postman/main.go
+++ cmd/postman/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	var (
 		workingDir  = flag.String("working-dir", ".", "project root directory")
-		commandPath = flag.String("command-path", "cmd/generate-postman/main.go", "target path for generated command")
+		commandPath = flag.String("command-path", "cmd/postman-generator/main.go", "target path for generated command")
 		importPath  = flag.String("import-path", "github.com/learncodexx/autogenpostman", "go import path for generator package")
 		outputPath  = flag.String("output", "cmd/postman/postman_collection.json", "default postman collection output path")
 		collection  = flag.String("collection-name", "API", "default collection name")
